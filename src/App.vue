76b6fc729c49b9fcb3f3ddeb045b1d706d99f77e
Fabio <thebloodshadow14@gmail.com>
2023-11-03 16:55:53 +0100

    ADD - AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import AppHero from "./components/AppHero.vue";
+export default {
+  data() {
+    return {
+      msg: "La mia prima app con Vue",
+    };
+  },
+  components: { AppHero },
+};
 </script>
 
 <template>
+  <AppHero />
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <span>{{ msg }}</span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+div {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import imageSrc from "../assets/sunset.jpeg";
+
+export default {
+  data() {
+    return {
+      imageSrc,
+    };
+  },
+};
+</script>
+
+<template>
+  <img :src="imageSrc" />
+</template>
+
+<style scoped>
+img {
+  height: 600px;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
